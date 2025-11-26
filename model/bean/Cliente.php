80a5a3f7e5af910b87ac20a88e889eb7118eb58e
mariag122 <felipedossantos2006@gmail.com>
2025-11-26 16:13:13 -0300

    Create Cliente class with attributes and methods

---
+++ model/bean/Cliente.php
@@ -0,0 +1,40 @@
+<?php
+    class Cliente {
+        //Atributos
+        private $id;
+        private $nome;
+        private $telefone;
+         private $email;
+
+        //Métodos
+        public function getId(){
+            return $this ->id;
+        }
+        public function setId($id) {
+            $this -> id = $id;
+        }
+          public function getNome(){
+            return $this ->nome;
+        }
+        public function setNome($nome) {
+            $this -> nome =$nome;
+        }
+          
+        public function getTelefone(){
+            return $this -> telefone;
+        }
+        public function setTelefone($telefone){
+            $this ->telefone = $telefone;
+        }
+
+        public function getEmail(){
+            return $this -> email;
+        }
+        public function setEmail($email){
+            $this ->email = $email;
+        }
+        //
+        public function __toString(){
+            return $this->nome;
+        }
+    }
