770b11ace26d8aafe2786fcd94dbce057030391c
mariag122 <felipedossantos2006@gmail.com>
2025-11-26 16:06:01 -0300

    Delete views/fornecedor/cliente/destroy.php

--- views/fornecedor/cliente/destroy.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require "../../autoload.php";
-
-    // Excluir do Banco de Dados
-    $dao = new ClienteDAO();
-    $dao->destroy($_GET['id']);
-
-    // Redirecionar para o index (Comentar quando não funcionar)
-    header('Location: index.php');
